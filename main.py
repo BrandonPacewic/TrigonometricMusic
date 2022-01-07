856998e47a69380520ae426640e642f8c18ea59c
Brandon <pacewicbrandon@gmail.com>
2022-01-07 09:59:49 -0800

    having problems with bottom bound

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from typing import List, Tuple
 import pygame
 
 class window:
-    HEIGHT = int(1920)
-    DISPLAY = (HEIGHT, int(HEIGHT/(16/9)))
+    WIDTH = int(1920)
+    HEIGHT = int(WIDTH/(16/9))
+    DISPLAY = (WIDTH, HEIGHT)
 
 
 FPS = 60
@@ -12,47 +13,47 @@ pygame.display.set_caption("Trigonometric Music")
 
 
 class box:
-    def __init__(self, x, y, width, height):
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
         self.x = x
         self.y = y
         self.w = width
         self.h = height
         self.pyRect = pygame.Rect(x, y, width, height)
 
-    def draw(self, win):
-        pygame.draw.rect(win, (255, 255, 255), self.pyRect)
+    def draw(self, win: Tuple[int, int]) -> None:
+        COLOR = (255, 255, 255)
+        pygame.draw.rect(win, COLOR, self.pyRect)
 
 
 def _create_bounds(win: Tuple[int, int]) -> List[box]:
-    THICKNESS = 10
-
-    bounds = [
+    THICKNESS = 5
+    return [
         box(0, 0, win[0], THICKNESS),
         box(0, 0, THICKNESS, win[1]),
-        box(0, win[1], win[0], THICKNESS),
+        box(0, win[1] - THICKNESS, win[0], THICKNESS),
         box(win[0] - THICKNESS, 0, THICKNESS, win[1]),
     ]
 
-    return bounds
 
-bounds = _create_bounds(window.DISPLAY)
-
-
-def render():
+def render(bounds: List[box]) -> None:
     WIN.fill((0, 0, 0))
     for bound in bounds:
         bound.draw(WIN)
 
+    pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
+    BOUNDS = _create_bounds(window.DISPLAY)
+
     while True:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type is pygame.QUIT:
                 exit()
 
-        render()
+        render(BOUNDS)
 
 
 if __name__ == '__main__':
