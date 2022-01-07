abb226a8e754cd6492ab91bad51ea314c2d45f79
Brandon <pacewicbrandon@gmail.com>
2022-01-07 09:14:24 -0800

    inital commit

---
+++ fixed/bounds.py
@@ -0,0 +1,2 @@
+class bounds:
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import pygame
+
+class window:
+    HEIGHT = 900
+    DISPLAY = (HEIGHT, HEIGHT/(16/9))
+
+
+FPS = 60
+WIN = pygame.display.set_mode(window.DISPLAY)
+
+bounds = []
+
+def render():
+    WIN.fill((0, 0, 0))
+    for bound in bounds:
+        bound.draw(WIN)
+
+
+def main():
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                exit()
+
+        render()
+
+
+if __name__ == '__main__':
+    main()
