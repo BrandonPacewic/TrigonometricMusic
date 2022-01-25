97ceda6a7fbf2fd5b51eaa7afc44626a7861f394
Brandon <pacewicbrandon@gmail.com>
2022-01-25 10:28:08 -0800

    added round class

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+#! /usr/bin/python3
+
 from typing import List, Tuple
+
+import math
 import pygame
 
 class window:
-    WIDTH = int(1600)
+    WIDTH = int(1820)
     HEIGHT = int(WIDTH/(16/9))
     DISPLAY = (WIDTH, HEIGHT)
 
 
 FPS = int(60)
 WIN = pygame.display.set_mode(window.DISPLAY)
-pygame.display.set_caption("Trigonometric Music")
+pygame.display.set_caption('Trigonometric Music')
 
 
 class box:
@@ -23,6 +27,17 @@ class box:
     def draw(self, win: Tuple[int, int]) -> None:
         COLOR = (255, 255, 255)
         pygame.draw.rect(win, COLOR, self.pyRect)
+
+
+class round:
+    def __init__(self, x: int, y: int, radius: int) -> None:
+        self.center = [x, y]
+        self.radius = radius
+        self.pyCircle = pygame.Circle(center, radius)
+
+    def draw(self, win: Tuple[int, int]) -> None:
+        COLOR = (255, 255, 255)
+        pygame.draw.circle(win, COLOR, self.pyCircle)
 
 
 def _create_bounds(win: Tuple[int, int]) -> List[box]:
@@ -45,7 +60,7 @@ def render(bounds: List[box]) -> None:
 
 def main():
     clock = pygame.time.Clock()
-    BOUNDS = _create_bounds(window.DISPLAY)
+    bounds = _create_bounds(window.DISPLAY)
 
     while True:
         clock.tick(FPS)
@@ -53,7 +68,7 @@ def main():
             if event.type is pygame.QUIT:
                 exit()
 
-        render(BOUNDS)
+        render(bounds)
 
 
 if __name__ == '__main__':
